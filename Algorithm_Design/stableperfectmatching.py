80a52e3794acc8e706ab22aef7b4776e4f3d039b
Faurby <lasse_fk@hotmail.com>
2023-09-05 10:22:08 +0200

    stable matching

--- Algorithm_Design/stableperfectmatching.py
+++ Algorithm_Design/stableperfectmatching.py
@@ -1,17 +1,48 @@
 n, m = map(int, input().split())
 
-# Stable marriage problem
-# insert preferences into dictionary
-people = list(input().split())
-
 preferences = {}
 matches = {}
+people = []
 for i in range(n):
-    preferences[people[0]] = people[1:]
-    matches[people[0]] = ""
-
-print("Preferences")
-print(preferences)
+    personAndTheirPreferences = list(input().split())
+    preferences[personAndTheirPreferences[0]] = (0, personAndTheirPreferences[1:])
+    matches[personAndTheirPreferences[0]] = ""
+    people.append(personAndTheirPreferences[0])
 
 proposers = people[0:n//2]
 
+def matchPairs(p, r):
+    matches[p] = r
+    matches[r] = p
+
+def rPrefersP(p, r, rPreferences):
+    return rPreferences.index(p) < rPreferences.index(matches[r])
+
+while proposers: # With great help from the legendary Anton Bertelsen
+    p = proposers.pop()
+    pIndex, pPreferences = preferences[p]
+
+    # check if p is free
+    if matches[p] != "":
+        continue
+    else:
+        r = pPreferences[pIndex]
+        rIndex, rPreferences = preferences[r]
+
+        # check if r is free
+        if matches[r] == "":
+            matchPairs(p, r)
+        else:
+            # check if r prefers p to their current match
+            if rPrefersP(p, r, rPreferences):
+                proposers.append(matches[r])
+                previousMatch = matches[r]
+                matches[previousMatch] = ""
+
+                matchPairs(p, r)
+            else: # r prefers their current match
+                preferences[p] = (pIndex + 1, pPreferences)
+                proposers.append(p)
+
+for i in range(n//2):
+    print(people[i] + " " + matches[people[i]])
